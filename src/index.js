06598a83da68e08f182951ee93c6c71966015b63
Agustín Rodríguez <me@agurodriguez.net>
2018-12-05 20:38:19 -0300

    return if no address is provided

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ const express = require('express');
 const app = express();
 
 app.get('/:address', (req, res) => {
-    let currency = cryptoaddress.detect(req.params.address);
-    cryptoaddress.balance(req.params.address, currency).then(balance => {
-        let o = {
-            address: req.params.address,
-            currency: currency,
-            balance: balance,
-        };
-        res.send(req.query.field ? o[req.query.field] : o);
-    });
+    if (!req.params.address) return;
+    var currency = cryptoaddress.detect(req.params.address);
+    cryptoaddress
+        .balance(req.params.address, currency)
+        .then(balance => {
+            var o = {
+                address: req.params.address,
+                currency: currency,
+                balance: balance,
+            };
+            res.json(req.query.field ? o[req.query.field] : o);
+        });
 });
 
 app.listen(process.env.PORT || 3000, () => {
