5064857c46b5153977a9082063af3061a650bb82
Agustín Rodríguez <me@agurodriguez.net>
2018-12-12 00:19:26 -0300

    added Bech32 address balance detection

--- src/currency/btc.js
+++ src/currency/btc.js
@@ -5,10 +5,10 @@ module.exports = {
     balance: function (address) {
         return new Promise((resolve, reject) => {
             https
-                .get(`https://blockchain.info/es/q/addressbalance/${address}`, (resp) => {
+                .get(`https://chain.so/api/v2/get_address_balance/BTC/${address}`, (resp) => {
                     let data = '';
                     resp.on('data', (chunk) => data += chunk);
-                    resp.on('end', () => resolve(JSON.parse(data)));
+                    resp.on('end', () => resolve(parseFloat(JSON.parse(data).data.confirmed_balance)));
                 })
                 .on('error', reject);
         });
